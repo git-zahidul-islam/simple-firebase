0b6dc04184d8cb158c47396a73c43d217020ec2c
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-03-30 18:01:12 +0600

    some function add

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,25 +1,65 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth'
 import app from '../../firebase/firebase.init';
+import { useState } from 'react';
 
 const Login = () => {
+    const [user, setUser] = useState(null)
     const auth = getAuth(app)
-    console.log(auth)
-    const provider = new GoogleAuthProvider()
+    // console.log(auth)
+    const googleProvider = new GoogleAuthProvider()
+    const githubProvider = new GithubAuthProvider()
+
 
     const handleGoogleAuthSignIn = () => {
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, googleProvider)
             .then(result => {
-                const user = result.user
-                console.log(user)
+                const loginUser = result.user
+                // console.log(loginUser)
+                setUser(loginUser)
             })
             .catch(error => {
                 console.log("error", error.message)
             })
     }
 
+    const handleGithubAuthSignIn = () => {
+        signInWithPopup(auth,githubProvider)
+        .then(result =>{
+            const loginUser = result.user
+            setUser(loginUser)
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    }
+
+    const handleGoogleAuthSignOut = () => {
+        signOut(auth)
+            .then(result => {
+                console.log(result)
+                setUser(null)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
     return (
         <div>
-            <button onClick={handleGoogleAuthSignIn}>Submit</button>
+            {user ?
+                <button onClick={handleGoogleAuthSignOut}>Sign Out</button> :
+                <>
+                    <button onClick={handleGoogleAuthSignIn}>Sign In google</button>
+                    <button onClick={handleGithubAuthSignIn}>Sign in Github</button>
+                </>
+            }
+
+            {user && <div>
+                <p>{user.displayName}</p>
+                <p>{user.email}</p>
+                <br />
+                <img src={user.photoURL} alt="" />
+            </div>}
         </div>
     );
 };
--- src/__step.js
+++ src/__step.js
@@ -1,16 +1,31 @@
 /**
+    -----------------------
+        Initial installation Setup
+    -----------------------
  * step 01: visit- console.firebase.google.com
  * step 02: create project (skip google Analytics)
  * step 03: Register app (create config)
  * step 04: install firebase : npm install firebase
  * step 05: add config file in your project
  * step 06: Danger, do not publish or make firebase config to public by pushing to those to github
+ *                  -----------
+ *                  INTEGRATION
+ *                  -----------
  * step 07: Go to Doc > build > authentication > web > get start
  * step 08: Export app from the firebase.config.js file: export default app
  * step 09: login.jsx import getAuth() from firebase/auth
  * step 10: create [ const auth = getAuth() ]
+ *                  ------------------
+ *                  Provider setup
+ *                  ------------------
  * step 11: import google auth provider & create a GoogleAuthProvider
  * step 12: use signInWithPopup & pass auth and provider
  * step 13: activate sign in method in firebase , (google,facebook,github);
- * 
+ * step 14: [vite]: change 127.0.0.1 to localhost
+ * --------------
+ * More Auth Provider 
+ * ---------------
+ * step 15: activate the auth provider (create app in github, provide redirect url from firebase , get client id & client secret in github and put firebase)
+ * step 16:
+ * step 17:
 */
